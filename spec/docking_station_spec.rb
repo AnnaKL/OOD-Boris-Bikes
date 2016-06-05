f7cc9c36f16e8fbce1de4f5393cac5ec7e96c49a
anna <anna.klimas@gmail.com>
2016-06-05 22:15:02 +0100

    Third and Fouth user stories completed.
    Member of public can return bike (dock it in the docking station).
    Also can check if there is already a bike in the docking station.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike)}
 
-  it 'releases a bike' do
+  it 'releases working  bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
--- spec/features/feature_public_accesses_the_bike_spec.rb
+++ spec/features/feature_public_accesses_the_bike_spec.rb
@@ -1,9 +1,17 @@
 require 'docking_station'
+require 'bike'
 
 feature 'member of public can access the bike' do
   scenario 'docking station releases a working bike' do
     docking_station = DockingStation.new
+    docking_station.dock Bike.new
     bike = docking_station.release_bike
-    bike.working?
+    expect(bike).to be_working
+  end
+  
+  scenario 'memeber of public can return a bike' do
+    bike = Bike.new
+    docking_station = DockingStation.new
+    expect(docking_station.dock(bike)).to eq bike
   end
 end
